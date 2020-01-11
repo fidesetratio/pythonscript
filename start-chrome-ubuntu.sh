9dc1530b348edcf09f67d9dcda9757566d6775f6
fidesetratio <patartimotiustambunan@gmail.com>
2020-01-11 08:47:37 +0700

    Create start-chrome-ubuntu.sh

---
+++ start-chrome-ubuntu.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#run Chrome via Selenium Server
+start-chrome() {
+    xvfb-run java -Dwebdriver.chrome.driver=/usr/local/bin/chromedriver -jar /usr/local/bin/selenium-server-standalone.jar
+}
+
+start-chrome-debug() {
+    xvfb-run java -Dwebdriver.chrome.driver=/usr/local/bin/chromedriver -jar /usr/local/bin/selenium-server-standalone.jar -debug
+}
+
+# Run Chrome Headless
+start-chrome-headless() {
+    chromedriver --url-base=/wd/hub
+}
+
+# Start
+start-chrome
+# start-chrome-debug
+# start-chrome-headless
